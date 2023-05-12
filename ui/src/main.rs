e7984c06c74fb2b0e2351c433104c7274a582e86
chi.zhang <chi.zhang@shangri-la.com>
2023-05-12 18:57:10 +0800

    feat: ui leptos

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -1,3 +1,22 @@
+use leptos::*;
+
 fn main() {
-    println!("Hello, world!");
+    mount_to_body(|cx| view! { cx, <App />})
+}
+
+#[component]
+fn App(cx: Scope) -> impl IntoView {
+    let (count, set_count) = create_signal(cx, 0);
+
+    view! {
+        cx,
+        <button
+            on:click=move |_| {
+                set_count.update(|x| *x += 1);
+            }
+        >
+            "click me:"
+            { move || count.get() }
+        </button>
+    }
 }
